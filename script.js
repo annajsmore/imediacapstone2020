98ccef5d94e13b955f717afd930869a9ca11d9c3
annajsmore <56464421+annajsmore@users.noreply.github.com>
2020-05-13 19:01:58 -0400

    Add files via upload
    Added website file

---
+++ script.js
@@ -0,0 +1,16 @@
+new fullpage('#fullpage', {
+	licenseKey: "EADE31E7-0A0A42C3-A91F5552-015353DF",
+	sectionsColor: ['#fffff', '#fffff', '#fffff', '#7bc3ea', '#b4985a', '#721216', '#9c9c9c', '#7bc3ea', '#7bc3ea'],
+	anchors: ['firstSlide', 'secondSlide', 'thirdSlide', 'fourthSlide', 'fifthSlide','sixthSlide','seventhSlide','eighthSlide'],
+  
+ 
+  	autoScrolling:true,
+	scrollHorizontally: true,
+	continuousHorizontal: true,
+	offsetSections: true,
+  	navigationTooltips: ['slide1', 'slide2'],
+	navigation: false,
+	slidesNavigation: true,
+	showActiveTooltip: false,
+	scrollbar: true,
+});
